bd254eac2734127ae7056e9a7b9f7cca5425c97a
Dileep <ddr.dileepraloti@gmail.com>
2024-09-25 17:20:35 +0530

    feat : addcommon input field

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,3 +1,63 @@
-export const ApInput = () => {
-  return <div>ApInput</div>;
+export const AppInput = ({
+  className,
+  onChange,
+  placeholder,
+  value,
+  label,
+  name,
+  type = "text",
+  disabled = false,
+  maxLength,
+  minLength,
+  pattern,
+  required = false,
+  autoFocus = false,
+  readOnly = false,
+  autoComplete = "off",
+}: IAppInput) => {
+  return (
+    <div className={`${className}`}>
+      {label && (
+        <label id={`${name}-label`} htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <input
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        type={type}
+        disabled={disabled}
+        maxLength={maxLength}
+        minLength={minLength}
+        pattern={pattern}
+        required={required}
+        autoFocus={autoFocus}
+        readOnly={readOnly}
+        autoComplete={autoComplete}
+        name={name}
+        id={name}
+        aria-labelledby={label && `${name}-label`}
+        aria-describedby={label && `${name}-description`}
+      />
+    </div>
+  );
 };
+
+interface IAppInput {
+  className: string;
+  onChange: (e: React.FormEvent<HTMLInputElement>) => any;
+  placeholder: string;
+  value: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  maxLength?: number;
+  minLength?: number;
+  pattern?: string;
+  required?: boolean;
+  autoFocus?: boolean;
+  readOnly?: boolean;
+  autoComplete?: string;
+  name?: string;
+}

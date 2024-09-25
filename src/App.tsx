26eda6053b6d5792d47d39b021e3f1cf430b4cf7
Dileep <ddr.dileepraloti@gmail.com>
2024-09-25 17:20:05 +0530

    feat : added login fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Login from "./pages/login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
---
+++ src/pages/login/loginFields.ts
@@ -0,0 +1,16 @@
+export const loginFields = [
+  {
+    name: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    disabled: false,
+    autoFocus: true,
+  },
+  {
+    name: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    disabled: false,
+    autoComplete: "current-password",
+  },
+];
